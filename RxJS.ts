957b5277e22e8450ed77dd3d3f9af9b7a8ab2b39
Reza Nikfal <62316360+rezanikfal@users.noreply.github.com>
2020-06-09 22:36:20 -0500

    RxJS Operators - 6/9/2020 night

--- RxJS.ts
+++ RxJS.ts
@@ -44,6 +44,62 @@
       err => console.log(`Error is ${err.name}`),
       () => console.log(`It is completed`))
       
+    // ****************** OPERATORS **********************
+
+    //"pipe" is a method in observables to chain operators as comma separated list
+    let source$ = of(1, 2, 3, 4, 5)
+
+    source$.pipe(
+      filter(val =>val>2),
+      map(val=>val*2)
+    ).subscribe(val=>console.log(val))
       
-      
-      
+    let beers = [
+      { name: 'Stella', country: 'Belgium', price: 9.5 },
+      { name: 'Sam Adams', country: 'USA', price: 8.5 },
+      { name: 'Bud Light', country: 'USA', price: 6.5 },
+      { name: 'Brooklyn Larger', country: 'USA', price: 8.0 },
+      { name: 'Sapporo', country: 'Japan', price: 7.5 }
+    ]
+
+    // "mergeMap" Convert Array of objects to individual objects 
+    // "tap" does not change the value. It provides a location to rub a small code
+    let reza$ = of(beers).pipe(
+      mergeMap(val => val),
+      filter(beer => beer.price > 8),
+      tap(beer => console.log(`the origin country is: ${beer.country}`))
+    ).subscribe(val => console.log(val)) 
+    
+    // ***** HANDLING ERRORS *****
+    // Using "catchError" to return an observable and subscribe to it to skip the error
+    let reza$ = of(beers).pipe(
+      mergeMap(val => val),
+      filter(beer => beer.price > 8),
+      tap(beer => console.log(`the origin country is: ${beer.country}`)),
+      catchError(err =>of({name: 'Unknown', country: 'Unknown', price: 0 }))
+    ).subscribe(
+      val => console.log(val),
+      err => console.log(`ERROR: ${err}`)
+    )
+
+    // Using "take" you specify the number of values you want. Also it "Completes" the observable
+    let reza$ = interval(1000)
+    reza$.pipe(
+      take(3)
+    ).subscribe(
+      val => console.log(val),
+      null,
+      () => console.log('All Done!'))
+
+    // Using "takeUntil" you continue to take values from source observable until the first
+    // value comes from the other observable (cancelTimer$). The completion handler also executed.
+    let button = document.getElementById('myButton')
+    let cancelTimer$ = fromEvent(button, 'click')
+
+    let reza$ = interval(1000)
+    reza$.pipe(
+      takeUntil(cancelTimer$ )
+    ).subscribe(
+      val => console.log(val),
+      null,
+      () => console.log('All Done!'))
